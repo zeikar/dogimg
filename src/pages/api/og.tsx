a23a233f93e39f4c046fcde902b99d21c0669986
zeikar <zeikar@naver.com>
2023-02-04 14:21:44 +0900

    Update image loading UI
    #6

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -92,7 +92,7 @@ export default async function handler(req: NextRequest) {
           <div
             style={{
               margin: "0px 40px 0px 40px",
-              height: "100px",
+              minHeight: "100px",
               maxHeight: "200px",
               fontSize: "70px",
               fontWeight: "700",
@@ -117,7 +117,6 @@ export default async function handler(req: NextRequest) {
       {
         width: 1200,
         height: 630,
-        debug: true,
       }
     );
   } catch (e: any) {
